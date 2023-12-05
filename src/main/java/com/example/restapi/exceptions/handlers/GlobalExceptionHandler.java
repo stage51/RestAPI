01f00495f09f9c1858eda0a79ee8a0d5ada272a0
merkulovandrej <sadzillaxxx@gmail.com>
2023-12-05 12:39:28 +0300

    update GlobalExceptionHandler for empty input

--- src/main/java/com/example/restapi/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/example/restapi/exceptions/handlers/GlobalExceptionHandler.java
@@ -1,14 +1,25 @@
 package com.example.restapi.exceptions.handlers;
 
 import com.example.restapi.exceptions.IllegalInputException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalInputException.class)
     public ResponseEntity<?> handleInputException(IllegalInputException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleHttpMessageNotReadable() {
+        return ResponseEntity.badRequest().body("Input can't be empty or null!");
     }
 }
